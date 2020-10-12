39371d3b414a2af0a9cd675101ecce165a254426
FrancescoDale <fd060862@gmail.com>
2020-10-12 14:37:54 +0200

    assegno valore alle variabili

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,7 @@
 var userName;
 var userSurname;
 var userColor;
+
+var userName = prompt(' inserire Nome: ');
+var userSurname = prompt( ' inserire Cognome: ');
+var userColor = prompt(' qual è il tuo colore preferito? :');
