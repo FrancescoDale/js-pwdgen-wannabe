3b12de189dcfa778babe0a4d67da57c91fe9ae02
FrancescoDale <fd060862@gmail.com>
2020-10-12 14:50:33 +0200

    output e fix di un problema che non faceva visualizzare la password generata ( due typo nel file main,.js )

--- assets/main.js
+++ assets/main.js
@@ -6,4 +6,4 @@ var userName = prompt(' inserire Nome: ');
 var userSurname = prompt( ' inserire Cognome: ');
 var userColor = prompt(' qual è il tuo colore preferito? :');
 
-document.getElementById('pwd').innerHTML =
+document.getElementById('pwd').innerHTML = document.getElementById('pwd').innerHTML + userName + userSurname + userColor + 20;
